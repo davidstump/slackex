8324e34e1b52ccaef503deb9d8468303ea8d392d
David Stump <david@davidstump.net>
2016-01-12 12:12:32 -0500

    Adds the ability to start a Slack Real Time Messaging session

---
+++ lib/slackex/rtm.ex
@@ -0,0 +1,14 @@
+defmodule Slackex.RTM do
+  @moduledoc """
+  Manage Slack Real Time Messaging Session
+  """
+
+  @doc """
+  This method starts a Real Time Messaging API session.
+  Refer to the RTM API documentation for full details on
+  how to use the RTM API.
+  """
+  def start(options \\ %{}) do
+    Slackex.request("rtm.start", params)
+  end
+end
