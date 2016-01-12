8d5d088305ada97f7041f4b82359167529f69578
David Stump <david@davidstump.net>
2016-01-12 12:25:48 -0500

    Adds ability to access team information and logs

---
+++ lib/slackex/team.ex
@@ -0,0 +1,29 @@
+defmodule Slackex.Team do
+  @moduledoc """
+  Get info on your team
+  """
+
+  @doc """
+  This method is used to get the access logs for
+  users on a team.
+  """
+  def access_logs(options \\ %{}) do
+    Slackex.request("team.accessLogs", options)
+  end
+
+  @doc """
+  This method provides information about your team.
+  """
+  def info do
+    Slackex.request("team.info")
+  end
+
+  @doc """
+  This method lists the integration activity logs for
+  a team, including when integrations are added, modified
+  and removed. This method can only be called by Admins.
+  """
+  def integration_logs(options \\ %{}) do
+    Slackex.request("team.integrationLogs", options)
+  end
+end
